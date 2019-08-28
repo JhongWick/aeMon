af3cb6a37029eb3a8724013734eda29c72d03b7f
orlando devera <deveraorlandojr@outlook.com>
2019-08-28 14:44:24 +0800

    Update

--- DataAccessLayer/RegionData.cs
+++ DataAccessLayer/RegionData.cs
@@ -34,5 +34,53 @@ namespace DataAccessLayer
                 return context.Regions.Where(o => o.RegionId == id).SingleOrDefault().Name;
             }
         }
+
+        public static bool Add(Region obj)
+        {
+            using (DBContext Context = new DBContext())
+            {
+                obj.UniqueId = Guid.NewGuid();
+                Context.Regions.Add(obj);
+                try
+                { Context.SaveChanges(); }
+                catch
+                { return false; }
+                return true;
+            }
+        }
+        public static bool Delete(Region obj)
+        {
+            bool result = false;
+            using (DBContext context = new DBContext())
+            {
+
+                Region region = context.Regions.SingleOrDefault(o=>o.RegionId == obj.RegionId);
+                if (region != null)
+                {
+                    context.Regions.Remove(region);
+                   
+                    context.SaveChanges();
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+        public static bool Edit(Region obj)
+        {
+            using (DBContext context = new DBContext())
+            {
+                Region region = context.Regions.Where(o => o.RegionId == obj.RegionId).SingleOrDefault();
+                region.Name = obj.Name;
+                region.RegionId = obj.RegionId;
+                region.Code = obj.Code;
+              
+                try
+                { context.SaveChanges(); }
+                catch
+                { return false; }
+                return true;
+            }
+        }
     }
 }
---
+++ KPIAnalytics/Controllers/RegionsController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using Entities;
+
+namespace KPIAnalytics.Controllers
+{
+
+    [KPIAuthorize]
+
+    [UserAuth]
+    public class RegionsController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Module
+        public ActionResult Index()
+        {
+            List<Region> model = new List<Region>();
+            model = RegionData.ListAll();
+            return View(model);
+        }
+        public ActionResult Add()
+        {   
+            List<Region> model = new List<Region>();
+            model = RegionData.ListAll();
+            int count = model.Count();
+            ViewBag.Number = count + 1;
+            return PartialView("Add", model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Region model)
+        {
+            bool result = RegionData.Add(model);
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int RegionId)
+        {
+            Region region = RegionData.Fetch(RegionId);
+            return PartialView("Delete", region);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Region model)
+        {
+            bool result = RegionData.Delete(model);
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int RegionId)
+        {
+            Region region = RegionData.Fetch(RegionId);
+            return PartialView("Edit", region);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Region model)
+        {
+            bool result = RegionData.Edit(model);
+            if (result)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
